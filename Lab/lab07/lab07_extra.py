7c27739cfbaa8e7a2a5b1ec1a0afbe7b43825542
Lucien <LucienXian97@gmail.com>
2017-08-23 21:15:00 +0800

    lab07

---
+++ Lab/lab07/lab07_extra.py
@@ -0,0 +1,106 @@
+from lab07 import *
+
+# Q6
+def reverse_other(t):
+    """Reverse the roots of every other level of the tree using mutation.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(5, [Tree(7), Tree(8)]), Tree(6)]), Tree(3)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(3, [Tree(5, [Tree(8), Tree(7)]), Tree(6)]), Tree(2)])
+    """
+    def reverse_helper(tr, need_reverse):
+        if t.is_leaf():
+            return
+        new_branch_root = [child.root for child in tr.branches][::-1]
+        for i in range(len(tr.branches)):
+            reverse_helper(tr.branches[i], not need_reverse)
+            if need_reverse:
+                tr.branches[i].root = new_branch_root[i]
+    reverse_helper(t, True)
+
+# Q7
+def cumulative_sum(t):
+    """Mutates t where each node's root becomes the sum of all entries in the
+    corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_sum(t)
+    >>> t
+    Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
+    """
+    for br in t.branches:
+        cumulative_sum(br)
+    t.root = sum([b.root for b in t.branches]) + t.root
+
+# Q8
+def deep_map_mut(fn, link):
+    """Mutates a deep link by replacing each item found with the
+    result of calling fn on the item.  Does NOT create new Links (so
+    no use of Link's constructor)
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> deep_map_mut(lambda x: x * x, link1)
+    >>> print_link(link1)
+    <9 <16> 25 36>
+    """
+    if link is Link.empty:
+        return
+    elif isinstance(link.first, Link):
+        deep_map_mut(fn, link.first)
+    else:
+        link.first = fn(link.first)
+    deep_map_mut(fn, link.rest)
+
+# Q9
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    links = []
+    while link is not Link.empty:
+        if link in links:
+            return True
+        links.append(link)
+        link = link.rest
+    return False
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    if link is Link.empty:
+        return False
+    slow, fast = link, link.rest
+    while fast is not Link.empty:
+        if fast.rest is Link.empty:
+            return False
+        elif slow == fast or slow == fast.rest:
+            return True
+        else:
+            slow, fast = slow.rest, fast.rest.rest
+    return False
+        
